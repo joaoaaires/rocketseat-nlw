e8e0dadd6a5306e057abaafe2d1dce149ad67ba6
Joao Aires <joao.aires@SV-NTB864.SEDE.SOVIS>
2023-05-16 15:37:04 -0300

    aula 01 - 30:09

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,9 @@
-console.log('oi');
+import fastify from "fastify";
+
+const app = fastify();
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log("HTTP server running on http://localhost:3333");
+});
