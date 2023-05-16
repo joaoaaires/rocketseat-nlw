e2a27da3a4d890f85ee92247c1b20645c1ddbc98
Joao Aires <joao.aires@SV-NTB864.SEDE.SOVIS>
2023-05-16 16:49:12 -0300

    aula 01 - 38:02

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("HTTP server running on http://localhost:3333");
-});
+app.get('/hello', () => 'Hello World!')
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
