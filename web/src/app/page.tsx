998e3615ae6e0480410323bba631edad20c919ce
Joao Aires <joao.aires@SV-NTB873.SEDE.SOVIS>
2023-05-17 18:07:09 -0300

    aula 01 - 1:13:08

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-2xl">Oi</div>
+  return (
+    <div className="w-20 bg-red-200 text-2xl font-bold uppercase text-cyan-400">
+      Oi
+    </div>
+  )
 }
