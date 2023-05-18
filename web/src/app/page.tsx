3bd49b709ecf0836804798cb96088a6b498b506e
Joao Aires <joao.aires@SV-NTB864.SEDE.SOVIS>
2023-05-18 09:40:33 -0300

    aula 02 - 22:58

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="">criar agora</a>!
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
